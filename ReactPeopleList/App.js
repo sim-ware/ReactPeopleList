e0ec029642b5d26f05a95d9159ad5c3c83588207
Samir Gossain <samir.gossain@gmail.com>
2018-09-12 14:15:35 +0100

    2 pages, firestore working// returns Array in console

--- ReactPeopleList/App.js
+++ ReactPeopleList/App.js
@@ -2,6 +2,49 @@ import React from 'react';
 import { Button, View, Text } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
+//################################################################################
+//# FIREBASE EXPERIMENT
+//################################################################################
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+firebase.initializeApp({
+  apiKey: "AIzaSyBjPYBiDN6nMmXDExQ2fPKg7k92npODy9c",
+  authDomain: "reactpeoplelist.firebaseapp.com",
+  projectId: "reactpeoplelist"
+});
+
+var db = firebase.firestore();
+
+db.settings({
+  timestampsInSnapshots: true
+});
+
+console.log('WHERE DOES THIS SHOW UP?');
+
+var data = []
+
+db.collection("people").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        data.push(doc.data().name);
+    });
+    data = data.sort()
+    console.log(data);
+});
+
+// console.log(data);
+
+// TODO: RETURN LIST OF NAMES, NOT GROUP OF DB OBJECTS
+
+// var config = {
+//     apiKey: "AIzaSyBjPYBiDN6nMmXDExQ2fPKg7k92npODy9c",
+//     authDomain: "reactpeoplelist.firebaseapp.com",
+//     databaseURL: "https://reactpeoplelist.firebaseio.com",
+//     projectId: "reactpeoplelist",
+//     storageBucket: "reactpeoplelist.appspot.com",
+//     messagingSenderId: "992090732552"
+//   };
+//################################################################################
 class HomeScreen extends React.Component {
   render() {
     return (
