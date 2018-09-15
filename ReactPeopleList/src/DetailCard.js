cfd9bb5a4a2e2903a6a5454564c70118c2ba7326
Samir Gossain <samir.gossain@gmail.com>
2018-09-15 21:46:13 +0100

    Removes some comments

--- ReactPeopleList/src/DetailCard.js
+++ ReactPeopleList/src/DetailCard.js
@@ -3,12 +3,7 @@ import { Text, View, StyleSheet} from 'react-native';
 import AgeTab from './AgeTab';
 import RatingTab from './RatingTab';
 import IDTab from './IDTab';
-// import { withNavigation } from 'react-navigation';
 const myModule = require('../Firebase');
-
-// var admin = require("firebase-admin");
-// var db = firebaseAdmin.database();
-// var ref = db.ref("people");
 
 
 class DetailCard extends Component {
@@ -20,23 +15,13 @@ class DetailCard extends Component {
     };
     this.componentDidMount = this.componentDidMount.bind(this);
   }
+
   componentDidMount() {
    const db = myModule.db;
    let name = String(this.props.name);
-   // console.log(name);
-   //
-   // Create a reference to the cities collection
    var citiesRef = db.collection("people");
-
-   // Create a query against the collection.
    name = name.replace(/['"]+/g, '');
-   // console.log(name);
    var query = citiesRef.where("name", "==", name);
-   // console.log(query);
-   // DYNAMIC: "\"Harry Hands\""
-   // HARD-CODED: "Adam Apple"
-   //
-   // db.collection('people').where("name", "==", name).get().then(collection => {
    db.collection('people').where("name", "==", name).get().then(collection => {
      const name = collection.docs.map(doc => doc.data().name)
      const rating = collection.docs.map(doc => doc.data().rating)
@@ -46,6 +31,7 @@ class DetailCard extends Component {
      this.setState({ birthday });
    });
    }
+
    render() {
       return (
              <View style = {styles.item}>
