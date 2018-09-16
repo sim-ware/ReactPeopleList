3a586acc90d0667c75dd50443a0192073621e6e6
Samir Gossain <samir.gossain@gmail.com>
2018-09-16 15:01:10 +0100

    edit mode changes rating a bit...

--- ReactPeopleList/src/RatingTab.js
+++ ReactPeopleList/src/RatingTab.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import { Button, Text, View, StyleSheet} from 'react-native';
 // import EditButton from './EditButton';
 // import EditMode from './EditMode';
+import { Icon } from 'react-native-elements'
+
+// <Icon
+//   name='rowing' />
 
 
 class RatingTab extends Component {
    constructor () {
      super();
      this.state = {
-       editMode:false
+       editMode:false,
+       newRating: 0
      };
      this.enterEditMode = this.enterEditMode.bind(this)
      this.leaveEditMode = this.leaveEditMode.bind(this)
+     this.increaseRating = this.increaseRating.bind(this)
+     this.decreaseRating = this.decreaseRating.bind(this)
    }
 
    enterEditMode() {
@@ -24,9 +31,25 @@ class RatingTab extends Component {
      this.setState({editMode:false});
    }
 
+   increaseRating() {
+     console.log('wooooooha')
+     console.log(this.props.rating)
+     this.setState({newRating: parseInt(this.props.rating[0] +1)});
+     console.log(this.state.newRating);
+     // this.props.rating += 1;
+     // console.log(this.props.rating += 1);
+   }
+
+   decreaseRating() {
+     console.log('yeeeee')
+     console.log(this.props.rating)
+     this.setState({newRating: parseInt(this.props.rating[0] -1)});
+     console.log(this.state.newRating);
+   }
+
    render() {
       const editButton = <View><Text>Rating: {this.props.rating}</Text><Button onPress={() => this.enterEditMode()} title="edit" /></View>;
-      const editMode = <View><Text>Rating: {this.props.rating}</Text><Button onPress={() => this.leaveEditMode()} title="cancel" /></View>;
+      const editMode = <View><View style = {styles.editMode}><Icon type='font-awesome' name='minus-square' onPress={() => this.decreaseRating()} /><Text>Rating: {this.state.newRating}</Text><Icon type='font-awesome' name='plus-square' onPress={() => this.increaseRating()} /></View><View><View style={styles.editMode}><Button onPress={() => this.leaveEditMode()} title="cancel" /><Button title="save" /></View></View></View>;
 
       return (
              <View style = {styles.item}>
@@ -41,6 +64,9 @@ const styles = StyleSheet.create ({
       paddingHorizontal: 50,
       margin: 2,
       alignItems: 'center',
+   },
+   editMode: {
+      flexDirection: 'row',
    }
 })
 
