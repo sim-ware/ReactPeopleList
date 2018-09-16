f4ae6c8631d72694b9857ad5679e62627626f379
Samir Gossain <samir.gossain@gmail.com>
2018-09-16 12:07:21 +0100

    Adds edit mode frame

--- ReactPeopleList/src/RatingTab.js
+++ ReactPeopleList/src/RatingTab.js
@@ -3,13 +3,39 @@ import { Button, Text, View, StyleSheet} from 'react-native';
 
 
 class RatingTab extends Component {
+   constructor () {
+     super();
+     this.state = {
+       editMode:false
+     };
+     this.enterEditMode = this.enterEditMode.bind(this)
+     this.leaveEditMode = this.leaveEditMode.bind(this)
+   }
+
+   enterEditMode() {
+     console.log('enteringEditMode')
+     console.log(this.state.editMode)
+     this.setState({
+      editMode: true
+     });
+   }
+
+   leaveEditMode() {
+     console.log(this.state.editMode)
+     this.setState({editMode:false});
+     console.log('leavingEditMode')
+     console.log(this.state.editMode)
+   }
+
    render() {
+      const editButton = <Button onPress={() => this.enterEditMode()} title="edit" />;
+      const editMode = <Button onPress={() => this.leaveEditMode()} title="cancel" />;
+      console.log('inRender')
+      console.log(this.state.editMode);
       return (
              <View style = {styles.item}>
                 <Text>Rating: {this.props.rating}</Text>
-                <Button
-                  title="edit"
-                />
+                {this.state.editMode ? editMode : editButton}
              </View>
       )
    }
